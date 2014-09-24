d12ea1d5c1f0c2681d2aea41861c65c9a3981893
Connor Linfoot <connorlinfoot@me.com>
2014-09-24 16:01:46 +0100

    Added Change Difficulty Feature

---
+++ src/com/connorlinfoot/adminplus/Handlers/ChangeDifficultyMenu.java
@@ -0,0 +1,51 @@
+package com.connorlinfoot.adminplus.Handlers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+public class ChangeDifficultyMenu {
+
+    public static Inventory changeDifficultyMenuInventory(Player p){
+        Inventory inv = Bukkit.createInventory( p,9,"Change Difficulty - AdminPlus" );
+
+        ItemStack is;
+        ItemMeta im;
+
+        is = new ItemStack(Material.MOB_SPAWNER,1);
+        im = is.getItemMeta();
+        im.setDisplayName(ChatColor.WHITE + "Peaceful");
+        is.setItemMeta(im);
+        inv.addItem(is);
+
+        is = new ItemStack(Material.MOB_SPAWNER,2);
+        im = is.getItemMeta();
+        im.setDisplayName(ChatColor.WHITE + "Easy");
+        is.setItemMeta(im);
+        inv.addItem(is);
+
+        is = new ItemStack(Material.MOB_SPAWNER,3);
+        im = is.getItemMeta();
+        im.setDisplayName(ChatColor.WHITE + "Normal");
+        is.setItemMeta(im);
+        inv.addItem(is);
+
+        is = new ItemStack(Material.MOB_SPAWNER,4);
+        im = is.getItemMeta();
+        im.setDisplayName(ChatColor.WHITE + "Hard");
+        is.setItemMeta(im);
+        inv.addItem(is);
+
+        return inv;
+    }
+
+    public static void openChangeDifficultyMenu(Player p){
+        p.openInventory(changeDifficultyMenuInventory(p));
+    }
+
+}
---
+++ src/com/connorlinfoot/adminplus/Listeners/ChangeDifficultyMenuListener.java
@@ -0,0 +1,39 @@
+package com.connorlinfoot.adminplus.Listeners;
+
+import org.bukkit.Difficulty;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class ChangeDifficultyMenuListener implements Listener {
+
+    @EventHandler
+    public void onMainMenuClick( InventoryClickEvent e ){
+        if( e.getInventory().getTitle().equals("Change Difficulty - AdminPlus") ){
+            if( e.getCurrentItem().getItemMeta().getDisplayName().contains("Peaceful") ){
+                if( e.getWhoClicked() instanceof Player ){
+                    Player p = (Player) e.getWhoClicked();
+                    p.getWorld().setDifficulty(Difficulty.PEACEFUL);
+                }
+            } else if( e.getCurrentItem().getItemMeta().getDisplayName().contains("Easy") ){
+                if( e.getWhoClicked() instanceof Player ){
+                    Player p = (Player) e.getWhoClicked();
+                    p.getWorld().setDifficulty(Difficulty.EASY);
+                }
+            } else if( e.getCurrentItem().getItemMeta().getDisplayName().contains("Normal") ){
+                if( e.getWhoClicked() instanceof Player ){
+                    Player p = (Player) e.getWhoClicked();
+                    p.getWorld().setDifficulty(Difficulty.NORMAL);
+                }
+            } else if( e.getCurrentItem().getItemMeta().getDisplayName().contains("Hard") ){
+                if( e.getWhoClicked() instanceof Player ){
+                    Player p = (Player) e.getWhoClicked();
+                    p.getWorld().setDifficulty(Difficulty.HARD);
+                }
+            }
+            e.setCancelled(true);
+        }
+    }
+
+}
